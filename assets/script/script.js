bba23545ac8a6b168a1f626f825c0a0337dc9a35
Oda Gabriela Undertun Antonsen <oda.antonsen@yahoo.com>
2019-02-28 16:02:37 +0100

    90%

---
+++ assets/script/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.images').click(function(){
+    });
+
+$(document).on('click','.responsive',function(){
+    //The element you have selected
+    var active = $(this)
+        console.log(active)
+    //Changed the size of the element you select once its into the display div
+    $('#increased').css({  
+            width: '100%',
+            heigh: 'auto' ,
+        });
+        //Removed the element in the display div
+    $('#increased').empty({
+    });
+        //Appends the image you selected into the display div
+    $('#increased').append(
+        '<img col-sm-7 responsive input" src="'+ active.prop('src') + '" alt="">');
+});
+
+
+
+
+
+});
