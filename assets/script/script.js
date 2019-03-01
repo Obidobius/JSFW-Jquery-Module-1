182406472668b0f92d830ba80ed0f81a8f7cd1c8
Oda Gabriela Undertun Antonsen <oda.antonsen@yahoo.com>
2019-03-01 12:24:57 +0100

    Done

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
+    
     $('.images').click(function(){
     });
+
 $(document).on('click','.mediaDisplay',function(){
         //The element you have selected
     var active = $(this)
@@ -13,20 +15,19 @@ $(document).on('click','.mediaDisplay',function(){
         });   
         //Finds the ID in the div you want to fade out. You add a function with what functions you want to be triggered after
             $('#increased').find('.input').fadeOut(
-                800,
+                600,
                 function(){
                     //Create the new image you have selected to be displayed in the div
-                    let newImg =  $('<img class="col-sm-7 mediaDisplay input" src="'+ active.prop('src') + '" alt="">')
+                    let newImg =  $('<img class="col-sm-7 input" src="'+ active.prop('src') + '" alt="">')
                     $(newImg).css({
                         display: 'none',
-                       
                     })
                     //Empty the current div to display new images
                     $('#increased').empty({
                     });
                     //Takes the new image into the div
                     $('#increased').append(newImg);
-                    $(newImg).fadeIn(800);    
+                    $(newImg).fadeIn(500);    
                 }
             );
     });
